0fa08114381cd561cd80631f7645dfbebd294c84
Xenia Tay <xeniatzy@gmail.com>
2015-03-22 22:46:38 -0400

    basic setup and login/logout functionality

--- a4/wassup/static/js/wassup.js
+++ a4/wassup/static/js/wassup.js
@@ -1,10 +1,20 @@
 window.addEventListener('load', function() {
-    // Place your Wassup app code here
-    console.log("Wassup?");
+
+    var loggedIn = hasCookie('user_id');
+    // User Authentication
+    // TODO user validation
+    if (loggedIn) {
+        // Redirect from login page to app
+        if ( location.pathname.match(/login/) ) {
+            location = location.origin;
+        }
+        document.body.classList.add('logged-in');
+    }
+
 });
 
 // Example derived from: https://developer.mozilla.org/en-US/docs/AJAX/Getting_Started
-function handleAjaxRequest() {
+function handleAjaxRequest(command) {
 
     // Create the request object
     var httpRequest = new XMLHttpRequest();
@@ -16,7 +26,7 @@ function handleAjaxRequest() {
         // 200 means we got an OK response from the server
         if (httpRequest.readyState === 4 && httpRequest.status === 200) {
             // Parse the response text as a JSON object
-            var responseObj = JSON.parse(httpRequest.responseText);
+            // var responseObj = JSON.parse(httpRequest.responseText);
 
             // TODO: Actually do something with the data returned
         }
@@ -30,6 +40,18 @@ function handleAjaxRequest() {
 
     // Send the JSON object, serialized as a string
     // TODO: You will need to actually send something and respond to it
-    var objectToSend = {};
+    var objectToSend = {
+        'command': command
+    };
+
     httpRequest.send(JSON.stringify(objectToSend));
+}
+
+/**
+ * Checks whether a cookie exists
+ * Derived from: https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
+ */
+function hasCookie(sKey) {
+    if (!sKey) { return false; }
+    return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
 }
